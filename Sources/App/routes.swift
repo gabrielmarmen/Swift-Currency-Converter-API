95fc062c3699c2e763b0b72b4664a977ed6eea47
Gabriel Marmen <gabton@outlook.com>
2022-11-12 19:04:47 +0700

    Edited routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,8 +6,14 @@ func routes(_ app: Application) throws {
         "It works!"
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("echange-rates","latest") { req async -> String in
+        "Latest rates not available yet."
+        //Add logic to get latest exchangerate from database
+    }
+    
+    app.get("echange-rates","one-hour") { req async -> String in
+        "The exchange rates are not available yet."
+        //Add logic to get latest exchangerate from database
     }
 
     try app.register(collection: TodoController())
