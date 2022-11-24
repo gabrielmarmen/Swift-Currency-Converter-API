cf1ce0dca48d08474889a66ec96576ab2f4d8b9c
Gabriel Marmen <gabton@outlook.com>
2022-11-24 21:16:42 +0700

    Added debugging lines, made multiple tests to try and figure out what is causing warnings in Xcode. Testing stability when GetLatestExchangeRatesJob fails to run properly

--- Sources/App/Jobs/GetLatestExchangeRateJob.swift
+++ Sources/App/Jobs/GetLatestExchangeRateJob.swift
@@ -14,24 +14,26 @@ struct GetLatestExchangeRateJob: AsyncScheduledJob {
     func run(context: Queues.QueueContext) async throws {
         
         let url = URL(string: "https://api.apilayer.com/exchangerates_data/latest?base=USD&apikey=30ZK03j1Mw4pdlxzG5c5ByhA1lGudvRj")
-            
+        
         do {
+            print("\(Date.debugTimeStamp) Pulling exchange rates ...")
             let (data, _) =  try await URLSession.shared.data(from: url!)
             let exchangeRates = try JSONDecoder().decode(ExchangeRate.self, from: data)
             let _ = try await exchangeRates.save(on: app.db)
             print("\(Date.debugTimeStamp) Pulled latest exchange rates and updated database.")
-            //if Environment.get("enableUpdatingExchangeRates") ?? "true" == "true"{
+            if Environment.get("enableUpdatingExchangeRates") ?? "true" == "true"{
                 app.queues.schedule(GetLatestExchangeRateJob(app: app))
                     .at(Date(timeIntervalSince1970: Date.now.timeIntervalSince1970 + 300))
                 try app.queues.startScheduledJobs()
                 print("\(Date.debugTimeStamp) Next update scheduled at \(Date(timeIntervalSince1970: Date.now.timeIntervalSince1970 + 300).formattedTime)")
-            //}
+            }
         }
         catch {
             app.queues.schedule(GetLatestExchangeRateJob(app: app))
-                .at(Date(timeIntervalSince1970: Date.now.timeIntervalSince1970 + 300))
+                .at(Date(timeIntervalSince1970: Date.now.timeIntervalSince1970 + 5))
             try app.queues.startScheduledJobs()
-            print("\(Date.debugTimeStamp) An error occured while getting latest exchange rates. Error: " + String(describing: error))
+            print("\(Date.debugTimeStamp) An error occured while getting latest exchange rates. Error: " + error.localizedDescription)
+            print("\(Date.debugTimeStamp) Trying again in 5 seconds ...")
         }
         
         
